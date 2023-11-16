71b9abae81dd4974fdaaedf277ebfdfab8c6739f
R S <rena_schott@gmx.de>
2023-11-16 16:55:35 +0000

    Change

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
-$("p").click(function() {
-    $("p").slideToggle("slow");
-})
+$(document).ready(function() {
+    $("p").click(function() {
+        $(this).slideToggle("slow");
+    });
+    
+    $("button").mouseenter(function() {
+        $(this).removeClass("makeRed").addClass("makeBlue");
+    });
+    
+    $("button").mouseleave(function() {
+        $(this).removeClass("makeBlue").addClass("makeRed");
+    });
+
+    $(".box").on("click", function() {
+        $(".box").on("click", function() {
+            /**
+             * When we click on an element that has
+             * a 'box' class, this event will be fired.
+             */
+            var classNames = $(this).attr("class").split(" ");
+            $("." + classNames[1]).css("background-color", "red");
+        });
+    });
+});
